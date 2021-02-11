947d0306939959e26d3df58f4b56d51ecbdcc9f9
D-coding-GH <daniel_hitchen@outlook.com>
2021-02-11 20:48:38 +0000

    descriptions added to code

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ const auth = require('./middlewares/auth');
 let authenticated = false;
 
 const app = express();
-dotenv.config({ path:'./.env' });
+dotenv.config({ path: './.env' });
 
-mongoose.connect( process.env.DB_URL, {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
-}).then( () => console.log("MongoDB is connected"));
+}).then(() => console.log("MongoDB is connected"));
 
 const viewsPath = path.join(__dirname, '/views');
 const publicDirectory = path.join(__dirname, '/public');
@@ -35,29 +35,35 @@ app.set('views', viewsPath);
 app.set('view engine', 'hbs');
 app.use(express.static(publicDirectory));
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extended: false }));
 app.use(cookieParser());
 
-app.get('/', async (req, res ) => { 
 
-    const allPosts = await Blogpost.aggregate([{$sample:{size:4}}])
-    ///........render blog post and user info to the page..
+///................homepage route renders blog posts and user info to the page for display
+
+app.get('/', async (req, res) => {
+
+    const allPosts = await Blogpost.aggregate([{ $sample: { size: 4 } }])
+
     console.log(allPosts)
-    
-    res.render('index',{
-        allPosts:allPosts
+
+    res.render('index', {
+        allPosts: allPosts
     });
 });
 
 
-app.get('/register', (req, res ) => { 
+app.get('/register', (req, res) => {
     res.render('register');
 });
 
+///................register user page, store all details including hashed password to the database
+//.................checks email doesnt not already exist and that passwors match
+
 app.post('/register', async (req, res) => {
     console.log(req.body);
-    // confirmPassword:req.body.confirmPassword
+
     const hashedPassword = await bcrypt.hash(req.body.userPassword, 8)
     const user = await User.find({ email: req.body.userEmail })
     const password1 = req.body.userPassword;
@@ -87,68 +93,12 @@ app.post('/register', async (req, res) => {
 }
 );
 
-app.get("/login", (req, res)=> {
+app.get("/login", (req, res) => {
     res.render("login")
 
 })
 
-// app.post("/login", auth.isLoggedIn, async (req, res) => {
-
-//     const user = await User.findOne({ email: req.body.userEmail });
-
-//     const isMatch = await bcrypt.compare(req.body.userPassword, user.password)
-//     console.log(user.admin)
-//     if (isMatch) {
-
-
-
-
-//         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//             expiresIn: process.env.JWT_EXPIRES_IN
-//         });
-
-//         console.log(token)
-
-//         const cookieOptions = {
-//             expires: new Date(
-//                 Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-//             ),
-//             httpOnly: true
-
-//         }
-
-//         res.cookie('jwt', token, cookieOptions)
-
-//         if (user.admin) {
-
-//             res.render('adminProfile', {
-//                 user: user,
-//                 id: user._id
-//             })
-
-//         } else {
-
-//             res.render('profile', {
-//                  user: user,
-//                 id: user._id
-//             })
-//         }
-
-
-//     } else {
-//         const errorMessage = "Details are wrong";
-
-//         res.render('login', {
-//             message: errorMessage
-//         })
-//     }
-
-// })
-
-
-app.get("/login", (req, res) => {
-    res.render("login");
-});
+///................login in user to site, creates JWT cookie, directs to user profile
 
 app.post("/login", async (req, res) => {
     const user = await User.findOne({ email: req.body.userEmail });
@@ -173,32 +123,28 @@ app.post("/login", async (req, res) => {
     }
 });
 
+///................route for profile page, will check if user is admin and if needed redirect to admin profile
 
 
 app.get("/profile", auth.isLoggedIn, async (req, res) => {
-   
-   
-    if (req.userFound && req.userFound.admin) {
-        const userDB = req.userFound; 
-        
-        console.log(req.userFound)
-        res.render("adminProfile",{
-            
-            user: userDB,
-           
-          
 
-            
-            
+
+    if (req.userFound && req.userFound.admin) {
+        const userDB = req.userFound;
+
+        console.log(req.userFound)
+        res.render("adminProfile", {
+
+            user: userDB,
+
         })
     } else if (req.userFound) {
-        const userDB = req.userFound; 
+        const userDB = req.userFound;
         console.log(userDB);
         res.render("profile", {
-          
+
             user: userDB,
-            
-           
+
         });
     } else {
         res.redirect("login");
@@ -206,47 +152,46 @@ app.get("/profile", auth.isLoggedIn, async (req, res) => {
 });
 
 
-
-
-app.get("/allUsers", (req, res)=> {
-res.render("allUsers")
-
+app.get("/allUsers", (req, res) => {
+    res.render("allUsers")
 })
+
+///................all users on the database
 
 app.post('/allUsers', async (req, res) => {
 
-        
-        const usersDB = await User.find({})
-    
-        console.log(usersDB)
-        res.render('allUsers',{ 
-            
-            users:usersDB 
-        })
-       
+
+    const usersDB = await User.find({})
+
+    console.log(usersDB)
+    res.render('allUsers', {
+
+        users: usersDB
+    })
+
 })
 
-app.post("/delete/:id", auth.isLoggedIn, async (req, res) => {
-    // const userId = req.userFound._id//...double check this
-    if (req.userFound.admin) {
-  
+///................admin delete users profile from site/database
 
-    await Blogpost.deleteMany({userid: req.params.id})    
-    await User.findByIdAndDelete( req.params.id);
-}
+app.post("/delete/:id", auth.isLoggedIn, async (req, res) => {
+
+    if (req.userFound.admin) {
+
+
+        await Blogpost.deleteMany({ userid: req.params.id })
+        await User.findByIdAndDelete(req.params.id);
+    }
     res.send("its been deleted")
-    // {
-    //     userId:userId//.....double check this
-    // });
-    // });
+
 });
 
+///................admin delete users posts from database
 
 app.post("/deleteUsersPost/:id", auth.isLoggedIn, async (req, res) => {
 
-    if(req.userFound.admin){
+    if (req.userFound.admin) {
 
-    await Blogpost.findByIdAndDelete(req.params.id)
+        await Blogpost.findByIdAndDelete(req.params.id)
 
     }
     res.send("post has been deleted")
@@ -254,68 +199,63 @@ app.post("/deleteUsersPost/:id", auth.isLoggedIn, async (req, res) => {
 })
 
 
+///................route to user details update
 
-
-app.get('/usersDetails/:id',auth.isLoggedIn, async (req,res) => {
+app.get('/usersDetails/:id', auth.isLoggedIn, async (req, res) => {
 
     const userId = await User.findById(req.params.id)
-
-
-
-    res.render("allUsersUpdate",{ 
-        userId:userId
+    res.render("allUsersUpdate", {
+        userId: userId
     })
 
 })
 
 
-
+///................route for admin only to update user details
 
 app.post("/allUsersUpdate/:id", auth.isLoggedIn, async (req, res) => {
-    
+
     const userId = req.params.id
     if (req.userFound.admin) {
-  
-        await User.findByIdAndUpdate(userId , {
+
+        await User.findByIdAndUpdate(userId, {
             name: req.body.userName,
             email: req.body.userEmail,
-           
+
         });
-        }
+    }
+    res.send("its been updated")
 
-
-        res.send("its been updated")
-       
 })
 
+///................all the users posts displayed to page
 
+app.get('/allUsersPosts', auth.isLoggedIn, async (req, res) => {
 
-app.get('/allUsersPosts', auth.isLoggedIn, async (req,res) => {
-    
     const userId = req.userFound._id
     const id = await User.findById(userId)
     const allusersPosts = await Blogpost.find({ userId: req.params.id })
-  
+
     console.log(allusersPosts)
 
-    res.render("allUsersPosts",  {
-    allPosts:allusersPosts
+    res.render("allUsersPosts", {
+        allPosts: allusersPosts
     })
-    
+
 })
 
 
+///................route/link to users update details page
 
-
-
-
-
-app.get('/update', auth.isLoggedIn, async (req,res) => {
+app.get('/update', auth.isLoggedIn, async (req, res) => {
     const userId = req.userFound._id
     const id = await User.findById(userId)
     res.render("update")
-
 })
+
+
+///................update users detail on the database, includes email comparison check/password check
+
 
 app.post("/updateUser", auth.isLoggedIn, async (req, res) => {
 
@@ -348,125 +288,121 @@ app.post("/updateUser", auth.isLoggedIn, async (req, res) => {
             message: message
         })
     }
+});
 
 
-}
+///................route/link for user to create new blog post
 
-);
+app.get('/newPost', auth.isLoggedIn, async (req, res) => {
 
-    
-
-
-app.get('/newPost', auth.isLoggedIn, async (req,res) => {
-    
     const userId = req.userFound._id
     const id = await User.findById(userId)
 
 
-    res.render('newPost') 
-    
-    
+    res.render('newPost')
+
+
 })
 
-app.post('/blogpost',  auth.isLoggedIn, async (req,res) => {
+///................create new blog post and store on database
+
+app.post('/blogpost', auth.isLoggedIn, async (req, res) => {
 
     const userId = req.userFound._id
     const id = await User.findById(userId)
 
-
-   
     await Blogpost.create({
-        
+
         title: req.body.postTitle,
         body: req.body.postBody,
         userid: userId,
-        
+
     });
-   const message = "blog updated"
-   res.render("allPosts", {
-    message:message,
-   })
-      
-   
+    const message = "blog updated"
+    res.render("allPosts", {
+        message: message,
+    })
+
+
 })
 
+///................all user blog post
 
-app.get('/allPosts', auth.isLoggedIn, async (req,res) => {//.....rendering all blog post for all users
-    
+app.get('/allPosts', auth.isLoggedIn, async (req, res) => {
+
     console.log(req.userFound)
-    const allPosts = await Blogpost.find({ userid:req.userFound })
+    const allPosts = await Blogpost.find({ userid: req.userFound })
     let firstObject = allPosts[0]
     console.log(allPosts)
 
-    res.render("allPosts",  {
-    allPosts:allPosts,
-    firstObject:firstObject
+    res.render("allPosts", {
+        allPosts: allPosts,
+        firstObject: firstObject
     })
-    
-    
+
+
 })
 
 
+///................route/link to update blog post, renders all post to page
+
+app.get('/updatePost', auth.isLoggedIn, async (req, res) => {
 
 
-app.get('/updatePost', auth.isLoggedIn, async(req,res) =>{
-
-    
-    const updatePost = await Blogpost.find({ userid:req.userFound._id })
-    // console.log(req.Blogpost._id)
+    const updatePost = await Blogpost.find({ userid: req.userFound._id })
 
     res.render('updatePost', {
-        updatePost:updatePost
-    }) 
+        updatePost: updatePost
+    })
 
 })
+
+///................update blog post page 
 
 app.post("/changePost/:id", auth.isLoggedIn, async (req, res) => {
 
-    const userId = req.userFound._id    
+    const userId = req.userFound._id
 
-    if(req.userFound) {
-    await Blogpost.findByIdAndUpdate(req.params.id, {
-        title: req.body.postTitle,
-        body: req.body.postBody,
-        userid: userId,
-    });
+    if (req.userFound) {
+        await Blogpost.findByIdAndUpdate(req.params.id, {
+            title: req.body.postTitle,
+            body: req.body.postBody,
+            userid: userId,
+        });
     }
     res.send("blog updated")
-    
-    })
+
+})
+
+///................delete users profile from site and database
 
 
-
-  //.....delete blog route needs creating and installing  
-
-
-
-app.post("/delete", auth.isLoggedIn, async (req,res) =>{//......render homepage with message
+app.post("/delete", auth.isLoggedIn, async (req, res) => {
 
     await User.findByIdAndDelete(req.userFound._id);
 
     const message = "profile DELETED";
 
     res.render('index', {
-        
-        message:message
+
+        message: message
     })
 
 })
 
+///................logout, with cookie expires date
 
-app.get("/logout", auth.logout, (req,res) => {
+app.get("/logout", auth.logout, (req, res) => {
 
     const message = "see you again soon";
-
-    res.render("index",{
-        message:message
+    res.cookie('jwt', { expires: 0 });
+    res.render("index", {
+        message: message
     })
 
 
 
-  }) 
+})
 
 
 app.listen(5500, () => {
@@ -478,5 +414,5 @@ app.listen(5500, () => {
 
 
 
-    
-    
+
+
